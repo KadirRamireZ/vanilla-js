2900bb14ffb783415cf0cad1a47b10d28778073b
Axl Kadir Ramirez Rodriguez <akramirezr@gmail.com>
2023-06-21 15:38:29 -0600

    Ejercicio4

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 function obtenerHoraActual() {
-    let fecha = new Date();
-    let hora = fecha.getHours();
-    let minutos = fecha.getMinutes();
-    let segundos = fecha.getSeconds();
-  
-    return hora + ":" + minutos + ":" + segundos;
-  }
-  
-  function actualizarHora() {
-    let horaElemento = document.getElementById("hora");
-    horaElemento.textContent = obtenerHoraActual();
-  }
-  
-  setInterval(actualizarHora, 1000);
-  
-  obtenerListaUsuarios();
-  
-  function obtenerListaUsuarios() {
-    fetch("https://api.hacienda.go.cr/fe/ae?identificacion=604400254")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Lista de usuarios:", data);
-      })
-      .catch((error) => {
-        console.error("Error al obtener la lista de usuarios:", error);
-      });
-  }
-  
-  function usuarios() {
-    return new Promise(obtenerListaUsuarios);
-  }
-  
+  let fecha = new Date();
+  let hora = fecha.getHours();
+  let minutos = fecha.getMinutes();
+  let segundos = fecha.getSeconds();
+
+  return hora + ":" + minutos + ":" + segundos;
+}
+
+function actualizarHora() {
+  let horaElemento = document.getElementById("hora");
+  horaElemento.textContent = obtenerHoraActual();
+}
+
+setInterval(actualizarHora, 1000);
+
+obtenerListaUsuarios();
+
+function obtenerListaUsuarios() {
+  fetch("https://api.hacienda.go.cr/fe/ae?identificacion=604400254")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Lista de usuarios:", data);
+    })
+    .catch((error) => {
+      console.error("Error al obtener la lista de usuarios:", error);
+    });
+}
+
+function usuarios() {
+  return new Promise(obtenerListaUsuarios);
+}
