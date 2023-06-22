3492b39e69c8d94be0293c0594e49d8f8cb67c53
Axl Kadir Ramirez Rodriguez <akramirezr@gmail.com>
2023-06-22 15:31:02 -0600

    agregar ejercicio 5

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-// Inserte el código aquí
+import { calculadora } from "./calculadora.js";
 
+function calcular(operacion, numero1, numero2) {
+  let resultado;
+  if (operacion === "sumar") {
+    resultado = calculadora.sumar(numero1, numero2);
+  }
+  console.log(resultado);
+  return resultado;
+}
+
+calcular("sumar", 1, 2);
